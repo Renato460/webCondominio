80500ff3d6a144d110cddbf952c20d4914fda0de
Renato <renato.reyes.valencia@gmail.com>
2021-07-07 16:01:15 -0400

    Prueba de integracion

---
+++ src/main/java/webCondominio/action/ActionNotification.java
@@ -0,0 +1,30 @@
+package webCondominio.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ActionNotification extends ActionSupport {
+    @Override
+    public String execute(){
+        System.out.println(this.api_version+"---"+this.notification_token);
+        return SUCCESS;
+    }
+
+    private String api_version;
+    private String notification_token;
+
+    public String getApi_version() {
+        return api_version;
+    }
+
+    public void setApi_version(String api_version) {
+        this.api_version = api_version;
+    }
+
+    public String getNotification_token() {
+        return notification_token;
+    }
+
+    public void setNotification_token(String notification_token) {
+        this.notification_token = notification_token;
+    }
+}
